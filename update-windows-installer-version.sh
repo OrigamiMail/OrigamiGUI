cfaabe4f395a8e5ff514492842f446c82250b5a6
Travis Pessetto <travis@pessetto.com>
2022-08-05 17:22:28 -0600

    Remove windows files (javafx library no longer needed need to up jre to 17)

--- update-windows-installer-version.sh
+++ update-windows-installer-version.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-version=`cat ./src/main/resources/VERSION`
-echo "version is $version"
-letterFreeVersion=`echo $version | sed -e "s/v//g"`
-echo "letter free version is $letterFreeVersion"
-echo "Update launch4j.cfg.xml"
-sed -i 's/{version}/$letterFreeVersion/g' ./launch4j.cfg.xml
-echo "Update install.nsi"
-sed -i 's/{version}/$letterFreeVersion/g' ./install.nsi
+#!/bin/bash
+version=`cat ./src/main/resources/VERSION`
+echo "version is $version"
+letterFreeVersion=`echo $version | sed -e "s/v//g"`
+echo "letter free version is $letterFreeVersion"
+echo "Update launch4j.cfg.xml"
+sed -i 's/{version}/$letterFreeVersion/g' ./launch4j.cfg.xml
+echo "Update install.nsi"
+sed -i 's/{version}/$letterFreeVersion/g' ./install.nsi
