a4bb9f5e9814e0d8ea77f2f23612855c31b25c00
Travis Pessetto <travis@pessetto.com>
2022-08-05 17:34:16 -0600

    update letter free version problem

--- update-windows-installer-version.sh
+++ update-windows-installer-version.sh
@@ -4,6 +4,6 @@ echo "version is $version"
 letterFreeVersion=`echo $version | sed -e "s/v//g"`
 echo "letter free version is $letterFreeVersion"
 echo "Update launch4j.cfg.xml"
-sed -i 's/{version}/$letterFreeVersion/g' ./launch4j.cfg.xml
+sed -i "s/{version}/$letterFreeVersion/g" ./launch4j.cfg.xml
 echo "Update install.nsi"
-sed -i 's/{version}/$letterFreeVersion/g' ./install.nsi
+sed -i "s/{version}/$letterFreeVersion/g" ./install.nsi
