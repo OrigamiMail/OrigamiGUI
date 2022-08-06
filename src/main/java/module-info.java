9d3802e892b681350d679fe25b834bb55357403d
Travis Pessetto <travis@pessetto.com>
2022-08-06 00:53:28 -0600

    Add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module com.pessetto.OrigamiGUI {
+    requires javafx.base;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires java.desktop;
+    requires mail;
+    requires com.pessetto.origamismtp;
+    requires javafx.web;
+    requires javafx.swing;
+    requires jdk.jsobject;
+}
