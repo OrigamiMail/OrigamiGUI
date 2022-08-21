b8aed172f4b5bb996448b0cb47a0bfda81bd1a50
Travis Pessetto <travis@pessetto.com>
2022-08-21 15:11:29 -0600

    Restore debug console

--- src/main/java/com/pessetto/origamigui/debug/DebugLogSingleton.java
+++ src/main/java/com/pessetto/origamigui/debug/DebugLogSingleton.java
@@ -17,8 +17,8 @@ public class DebugLogSingleton
 	
 	protected DebugLogSingleton()
 	{
-		//redirectSystemOut();
-		//redirectSystemErr();
+		redirectSystemOut();
+		redirectSystemErr();
 	}
 	
 	public static DebugLogSingleton getInstance()
